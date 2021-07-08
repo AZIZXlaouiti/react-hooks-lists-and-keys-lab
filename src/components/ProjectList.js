366f57878faf6f74a98a2f6359ebb866b3969194
AZIZXlaouiti <azizrocke@gmail.com>
2021-07-08 10:51:39 -0400

    Completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,7 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projects.map(({ name, about, technologies })=><ProjectItem name={name} about={about} technologies={technologies} />)}</div>
     </div>
   );
 }
